664ae720764ad8afc70ec360cf2068ee05e9bda8
Hla <hlahijazi22@gmail.com>
2025-10-21 13:34:56 +0300

    Update admin.js
    fix secure cookie issue

--- BackEnd/config/admin.js
+++ BackEnd/config/admin.js
@@ -10,4 +10,11 @@ module.exports = ({ env }) => ({
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
+  session: {
+    secret: env('ADMIN_COOKIE_SECRET', 'a-long-default-cookie-secret'),
+    cookie: {
+      // Set secure to false to allow cookies over HTTP
+      secure: false, 
+    },
+  },
 });
