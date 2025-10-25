8d91f6dd68c07784ed073f553052df8da97f189c
Hla <hlahijazi22@gmail.com>
2025-10-25 20:54:09 +0200

    Update admin.js
    fix issues

--- BackEnd/config/admin.js
+++ BackEnd/config/admin.js
@@ -1,20 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  session: {
-    secret: env('ADMIN_COOKIE_SECRET', 'a-long-default-cookie-secret'),
-    cookie: {
-      // Set secure to false to allow cookies over HTTP
-      secure: false, 
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
 });
