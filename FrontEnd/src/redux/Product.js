8e23407fb6437233699ec923d007bdd3f84d1f6d
hlahijazi2002 <hlahijazi22@gmail.com>
2025-10-26 18:07:03 +0200

    Update base URL to Render backend

--- FrontEnd/src/redux/Product.js
+++ FrontEnd/src/redux/Product.js
@@ -4,7 +4,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const ProductApi = createApi({
   reducerPath: "ProductApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:1337/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_BASE_URL}/api`,
+  }),
   endpoints: (builder) => ({
     getProductByName: builder.query({
       query: (name) => `${name}`,
